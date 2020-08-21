c96ca822e4c624c4417839e822012ed9e1963f6f
Felipe Milana <felipe.milana97@gmail.com>
2020-08-21 12:35:51 -0300

    Implementing Board and Piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,10 @@ public class Board {
 	
 	//constructors
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating the board: needs 1 row and 1 column at least!");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];   //instancia na variavel pieces uma matriz nula com linha e colunas informadas no construtor   
@@ -21,15 +25,64 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
+	
+	public Piece piece(int row, int column) {  //retorna a peça informando linha e coluna
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board!");  //nao estamos tratando, apenas lançando
+		}
+		return pieces[row][column];
+	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}	
+	public Piece piece(Position position) {   //retorna a peça informando a posição
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already piece in this position" + position);
+		}
+		
+		pieces[position.getRow()][position.getColumn()] = piece;  //adiciona a peça nessa posição da matriz
+		piece.position = position;  //posição deixa de ser nula;
+	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		
+		if(piece(position) == null) {  //se ja for nulo 
+			return null;
+		}
+		
+		Piece p = piece(position);  //adiciona na variavel p, o que estiver na posição informada
+		p.position = null;    //a posição da peça se torna nula
+		pieces[position.getRow()][position.getColumn()] = null;  //a posição ma matriz se torna nula
+		return p;		
+	}
+	
+	public boolean positionExists(Position position) {    //exceções
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		return pieces[position.getRow()][position.getColumn()] != null;
+	}
+	
+	private boolean positionExists(int row, int column) {   //de 0 a 7
+		return row >= 0 && row < rows && column >= 0 && column < columns;   //rows = columns = 8
+	}
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {  //peça no tabuleiro
+public abstract class Piece {  //peça no tabuleiro
 
 	//association
 	protected Position position;  //é apenas uma posição de matriz(inicialmente nula) e tera acesso a esse atributo, as subclasses dessa classe
@@ -14,5 +14,23 @@ public class Piece {  //peça no tabuleiro
 	//methods
 	protected Board getBoard() {   
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();   //metodo que retorna a matriz de movimentos de cada peça
+	
+	public boolean possibleMove(Position position) {   //informa uma posição e verifica se naquela posição tem true or false na matriz de movimentos da peça
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {  //verifica se tem pelo menos 1 movimento possivel
+		boolean[][] mat = possibleMoves();    //armazena na matriz mat os movimentos possiveis da peça
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {   //percorre a matriz
+				if(mat[i][j]) {   //se tiver true
+					return true;    //retorna que tem movimento possivel
+				}
+			}
+		}
+		return false;   //senao, retorna que nao tem movimento possivel
 	}
 }
